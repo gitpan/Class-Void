aeb1fd703eb064b641c6af9ffa366ceb1348c956
Malte Ubl <ubl@schaffhausen.de>
2001-07-17 09:38:03 +0000

    Import of MALTEU/Class-Void-0.03 from CPAN.
    gitpan-cpan-distribution: Class-Void
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MALTEU/Class-Void-0.03.tar.gz
    gitpan-cpan-author:       MALTEU
    gitpan-cpan-maturity:     released

--- Void/Void.pm
+++ Void/Void.pm
@@ -2,28 +2,33 @@ package Class::Void;
 use strict;
 use warnings;
 use vars qw($VERSION $Nothin $new); 
-$VERSION = '0.02';
+$VERSION = '0.03';
 
-use overload q("")     => sub { "" },         # stringify to the empty string
-            "nomethod" => sub { shift->new }; # return a new object for all other overloadable operators
+use overload q("")     => sub { "" },  # stringify to the empty string (returning undef is poised to create warnings)
+            "nomethod" => sub { shift->new }; # call new for all other overloadable operators
 
 $new = sub {
-	my $scalar;
-	$Nothin = bless \$scalar;
+# private sub so my name space doesnt get polluted which anything that could stop the AUTOLOAD sub from executing
+	my $scalar;            # allocate an empty scalar
+	bless \$scalar, shift; # bless it into the calling package, and return it
 };
 
 sub new {
-	defined $Nothin ? $Nothin : &$new # define $Nothin once, afterwards return it and never create another object in the same session
+	$Nothin = $new->(shift) unless defined $Nothin; # define $Nothin once
+	return $Nothin;
 }
 
-sub AUTOLOAD {
-	shift->new
+sub can {
+	my $class   = shift;
+	sub { $class->new }
 }
 
-sub DESTROY {}; # autoloading DESTROY would lead to an infinite loop because it would create
-                # a new object which in turn calls destroy immediately and so on
-                
-return q<I ain't seen nothin>
+sub AUTOLOAD { shift->new };
+
+sub DESTROY  {}; # autoloading DESTROY would lead to an infinite loop because it'd create
+                 # a new object which in turn calls destroy immediately and so on
+               
+q<I ain't seen nothin>
 
 __END__
 
@@ -32,14 +37,25 @@ __END__
 Class::Void - A class that stringifies to nothing for whatever you do with it
 
 =head1 SYNOPSIS
+	use Class::Void;
+  
+	my $object = Class::Void->new;
+  
+	$object->foo->bar("baz")->foo;
+	print $object->employees("bob_smith")->age;
+	print $object->bla("test")->foo->bar->baz * 2 / 2 * $object ** 8;
 
-  use Class::Void;
-  
-  my $object = Class::Void->new;
-  
-  $object->foo->bar("baz")->foo;
-  print $object->employees("bob_smith")->age;
-  print $object->bla("test")->foo->bar->baz * 2 / 2 * $object ** 8;
+	if(my $coderef = $object->can("sleep")) {
+		$coderef->()
+	}
+
+	package Foo;
+	use base "Class::Void";
+	sub new {
+		my $class = shift;
+		bless {}, $class;
+	}
+
 
 =head1 DESCRIPTION
 
@@ -49,14 +65,31 @@ against its instances. Stringification returns the empty string which means
 you can do pretty much everything with this module, in the end you always
 get nothing.
 
+=head2 Exceptions
+
+Did I use words like everything, always and all? Well, here are the exceptions:
+Methods defined in UNIVERSAL are not overridden, because these methods should
+have a common behavior for all classes. This means that your Class::Void objects
+will return true for $object->isa("Class::Void").
+
+B<can> is special. It always returns a coderef to a function which has the typical 
+behavior of methods defined in C<Class::Void>.
+
+=head2 Subclassing
+
+You may subclass C<Class::Void>. This might be especially useful to give
+some meaning to the objects internals by overriding "new" with a method
+that returns some kind of blessed reference other than a scalar.
+
 =head1 "Why the hell would I need that?"
 
 Everytime you have some class which isn't quite finished or which doesnt
 provide a way to quietly do nothing, you can use this module as a stub
-or a plug for the hole. It might be a web-based database maintenance
-tool, where you quickly need an empty page for new entries or a leave
-in a tree that needs something which doesnt die on method calls but which
-doesnt take much memory, either.
+or a plug for the hole. Replacing objects of this class with objects
+of another class can change a web page to change data to a page for new
+entries in seconds. Another suggested use is to plug dead ends in a tree 
+with something which doesn't die on method calls but which doesn't take much 
+memory, either.
 
 I'd be very interested in other uses, so please drop me a mail if you did
 something fancy with this module.
@@ -64,12 +97,6 @@ something fancy with this module.
 =head1 BUGS
 
 Using this module might be a bug :-) 
-Seriously, I need to think about a way to not only handle method calls
-and operations but also evil people who mess around with the underlying
-data structure. Of course, I could have separate sub-classes for blessed
-Arrays, Hashes, etc, but first I really don't know what a blessed RegEx
-should do in this context, and second if one messes around with encapsulation
-one should expect to shoot oneself in the foot.
 
 =head1 COPYRIGHT
 
